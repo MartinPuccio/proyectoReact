d9c5a95513406f032253f3afc58f11dc1f85a73b
Martin Puccio <martinpuccio30@gmail.com>
2022-04-18 23:03:34 -0300

    desafio menu ecommerce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/home/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <h1>Hola Mundo con React!</h1>
       </header>
